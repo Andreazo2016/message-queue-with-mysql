3288c1f3e3f769495555090fb9e6d796bace74e9
Andreazo Souza <andreazosouza@SKLM19.local>
2024-09-17 16:52:24 -0300

    feat: first commit

---
+++ init.db/db.sql
@@ -0,0 +1,12 @@
+USE queue_db;
+
+CREATE TABLE IF NOT EXISTS messages (
+  id  int auto_increment primary key,
+  payload json not null,
+  status varchar(100) default 'create'  not null,
+  queue_name varchar(255) not null,
+  scheduled_at timestamp not null,
+  scheduled_at_date_time datetime not null,
+  created_at     datetime     default CURRENT_TIMESTAMP not null,
+  updated_at     datetime     null
+);
